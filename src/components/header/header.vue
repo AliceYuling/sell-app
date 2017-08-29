a0c6c27d8bfb845c7c5d38e20695fb8ec9c0398e
AliceYuling <yyl1215@126.com>
2017-08-29 22:36:30 +0800

    first commit

---
+++ src/components/header/header.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="header">
+	header
+	</div>
+</template>
+
+<script>
+	export default{
+        components: {
+            name: 'header'
+        }
+    };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
